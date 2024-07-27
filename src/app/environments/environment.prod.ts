33e1684a74b2fcc9abe8cbccf7e82f33575e5a93
Zolouz966 <zrodrigo96@gmail.com>
2024-07-26 22:42:31 -0400

    we add environments

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  url_api: 'https://api.laboratorio-pasteur.com',
+  url_public: 'https://laboratorio-pasteur.com',
+};
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  // url_api: 'http://localhost:3000',
+  url_api: 'https://api.laboratorio-pasteur.com',
+  url_public: 'https://laboratorio-pasteur.com',
+};
